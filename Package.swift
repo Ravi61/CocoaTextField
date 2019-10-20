6114f2887a60d64591eddba876b893e43a0ae53f
Ravi Aggarwal <ravi.aggarwal61@gmail.com>
2019-10-20 15:30:31 +0530

    adding path

--- Package.swift
+++ Package.swift
@@ -23,9 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CocoaTextField",
-            dependencies: []),
+            dependencies: [],
+            path: "CocoaTextField"),
         .testTarget(
             name: "CocoaTextFieldTests",
-            dependencies: ["CocoaTextField"]),
+            dependencies: ["CocoaTextField"],
+            path: "CocoaTextFieldTest"),
     ]
 )
